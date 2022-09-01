5694a811d89a6628b60436fe8ecd7bf6e5b5a449
Emon <emonmnsu95@gmail.com>
2022-09-01 12:00:54 -0500

    todos function

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,28 @@
-import React, {useState} from 'react';
-import TodoForm from './TodoForm';
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { RiCloseCircleLine} from 'react-icons/ri';
+import { TiEdit} from 'react-icons/ti';
 
 
-const Todo = () => {
-  return (
-    <div>
-      
+const Todo = ({todos, completeTodo}) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+        <div key={todo.id} onClick={()=> completeTodo(todo.id)}>
+            {todo.text}
+        </div>
+        <div className="icons">
+            <RiCloseCircleLine />
+        </div>
     </div>
-  )
-}
+  ));
+};
 
-export default Todo
+export default Todo;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,36 @@
-import React, {useState} from 'react';
-import TodoForm from './TodoForm';
+import React, { useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
 
-    const [todos, setTodos] = useState([]);
-
-    const addTodo = todo => {
-        if(!todo.text || /^\s*$/.test(todo.text)){
-            return
-        }
-        const newTodos = [todo, ...todos];
-
-        setTodos(newTodos);
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
     }
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
     <div>
       <h1>Whats the plan for to the day</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} />
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
