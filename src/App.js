6050764be079a418f817d5af6f13ac60181714f0
Emon <emonmnsu95@gmail.com>
2022-09-01 12:47:52 -0500

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
-import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <h1>ToDo Application</h1>
-      <TodoForm/>
+    <div className="todo-app">
       <TodoList />
     </div>
   );
