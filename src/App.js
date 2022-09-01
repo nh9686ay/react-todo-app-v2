24c67d16c7759f501e5b00f577407e09d2fe595f
Emon <emonmnsu95@gmail.com>
2022-09-01 11:12:05 -0500

    handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
     <div className="App">
       <h1>ToDo Application</h1>
+      <TodoForm/>
     </div>
   );
 }
